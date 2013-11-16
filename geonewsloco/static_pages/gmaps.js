60ddce6477ba82282abf45a332ce1cfc974b6181
Andrew Tam <andrew.tam.sp@gmail.com>
2013-11-16 14:00:41 -0500

    changed zoom level to 14

--- geonewsloco/static_pages/gmaps.js
+++ geonewsloco/static_pages/gmaps.js
@@ -2,7 +2,7 @@ var map;
 function initialize() {
 var mapOptions = {
   center: new google.maps.LatLng(40.6700, -73.9400),
-  zoom: 8,
+  zoom: 14,
   mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 var map = new google.maps.Map(document.getElementById("map-canvas"),
