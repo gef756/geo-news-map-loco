5dbe17bcc25c307f3219a0d43cee6e33bc27c52d
Andrew Tam <andrew.tam.sp@gmail.com>
2013-11-16 14:07:43 -0500

    random stuff

--- geonewsloco/static_pages/gmaps.js
+++ geonewsloco/static_pages/gmaps.js
@@ -1,8 +1,8 @@
 var map;
 function initialize() {
 var mapOptions = {
-  center: new google.maps.LatLng(40.6700, -73.9400),
-  zoom: 14,
+  center: new google.maps.LatLng(40.714623,-74.006605 ),
+  zoom: 12,
   mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 var map = new google.maps.Map(document.getElementById("map-canvas"),
