bd69068857b0bb1261807c11dafd617f18410803
Robert Li <li.robertj+git@gmail.com>
2013-11-16 14:25:15 -0500

    pretty

--- geonewsloco/maploco/models.py
+++ geonewsloco/maploco/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Story(models.Model):
-    headline = models.CharField(max_length=100)
-    url = models.CharField(max_length=255)
-    lat = models.FloatField()
-    lon = models.FloatField()
+    headline             = models.CharField(max_length=100)
+    url                  = models.CharField(max_length=255)
+    lat                  = models.FloatField()
+    lon                  = models.FloatField()
     location_description = models.CharField(max_length=255)
-    popularity = models.IntegerField()
-    blurb = models.CharField(max_length=1000)
+    popularity           = models.IntegerField()
+    blurb                = models.CharField(max_length=1000)
