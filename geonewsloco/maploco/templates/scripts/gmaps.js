6d04351a20a36fb88f70df30ca729332f114306f
MatthewPhinney <matthew.d.phinney@gmail.com>
2013-11-16 17:06:27 -0500

    added infowindow to map marker

--- geonewsloco/maploco/templates/scripts/gmaps.js
+++ geonewsloco/maploco/templates/scripts/gmaps.js
@@ -61,11 +61,17 @@ function initialize() {
 }
 
 function addMarker() {
+        var infowindow = new google.maps.InfoWindow({
+              content: "Hello World"
+          });
 	var myLatlng = new google.maps.LatLng(40.714623,-74.006605);
 	var marker = new google.maps.Marker({
 	  position: myLatlng,
 	  map: map,
 	  title: 'Hello World!'
 	});
+        google.maps.event.addListener(marker, 'click', function() {
+                  infowindow.open(map,marker);
+                });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
