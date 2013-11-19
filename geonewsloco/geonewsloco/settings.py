efbe944ade62fc27d57520d6207c7f81e28e74f3
Gabe Fernando <gef756@gmail.com>
2013-11-18 21:33:53 -0500

    fix db settings

--- geonewsloco/geonewsloco/settings.py
+++ geonewsloco/geonewsloco/settings.py
@@ -95,7 +95,7 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 # Parse database config from $DATABASE_URL
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {'default': dj_database_url.config() }
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
